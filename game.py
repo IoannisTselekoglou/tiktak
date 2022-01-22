2d8d8a29c0b263b55a78636c12db3df2b7ab4241
Janni Tselekoglou <jannitselekoglou@Air-von-Janni.fritz.box>
2022-01-22 22:51:31 +0100

    Still verry buggy, trying to fix bot

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 list1 = [0,0,0,0,0,0,0,0,0]
 
 def gamefield():
@@ -16,24 +16,28 @@ def gamelogic(number):
 
 def move(number):
   while number > 10 or number < 0:
-    print("invalid input, type an intiger number between 1-9 each representing one position on the board where u want to make an cross\n ")
+    number = int(input("invalid input, type an intiger number between 1-9 each representing one position on the board where u want to make an cross\n "))
   if gamelogic(number) and list1[number-1] == (1 or 2):
-    number = int(input("type number bitch\n "))
-    move(number)
+      number = int(input("type number bitch\n "))
+      move(number)
   elif gamelogic(number) and list1[number-1] != 1:
-    list1[number-1] = 1
-    return True and print(gamefield())
+      list1[number-1] = 1
+  print(gamefield())
+  return botmove()
+
 
 
 def botmove():
-  botnumber = random.randrange(1,9,1) 
-  while gamelogic(botnumber)== False and list1[botnumber] == 1:
-    botnumber = random.randrange(1,9,1) 
-    if gamelogic(botnumber) and list1[botnumber] != 1:
-      list1[botnumber]
-
+  botnumber = random.randint(1,9) 
+  while list1[botnumber-1] == (1 or 2):
+    botnumber = random.randint(1,9) 
+    if gamelogic(botnumber) and list1[botnumber - 1] == 0:
+      list1[botnumber-1] = 2
+  print(gamefield())
+  return move(int(input("Your Turn, Bitch \n"))) 
 
 move(int(input("Type in number, representing spot on playingfield\n ")))
+
 
 
 
